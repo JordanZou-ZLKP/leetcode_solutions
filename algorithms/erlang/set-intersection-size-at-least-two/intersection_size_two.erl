0acde63a7a425d3d267a5ff967ef7d70a20398b9
Jordan Zou <iqiaodongzou@gmail.com>
2025-12-09 15:35:54 +0800

    🩹add new solution

--- algorithms/erlang/set-intersection-size-at-least-two/intersection_size_two.erl
+++ algorithms/erlang/set-intersection-size-at-least-two/intersection_size_two.erl
@@ -1,3 +1,23 @@
 -spec intersection_size_two(Intervals :: [[integer()]]) -> integer().
 intersection_size_two(Intervals) ->
-  .
+    SortFun = fun([S1, E1], [S2, E2]) ->
+        if
+            E1 < E2 -> true;
+            E1 > E2 -> false;
+            true -> S1 > S2
+        end
+    end,
+    SortedIntervals = lists:sort(SortFun, Intervals),
+    solve(SortedIntervals, -1, -1, 0).
+
+solve([], _, _, Acc) ->
+    Acc;
+solve([[S, E] | T], P1, P2, Acc) ->
+    if
+        S =< P1 ->
+            solve(T, P1, P2, Acc);
+        S =< P2 ->
+            solve(T, P2, E, Acc + 1);
+        true ->
+            solve(T, E - 1, E, Acc + 2)
+    end.
